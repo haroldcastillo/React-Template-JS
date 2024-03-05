8fd57940d826ff099b194017f8567f1402471019
mihkuno <caindayjoeninyo@gmail.com>
2024-03-05 14:31:27 +0800

    added account provider

--- client/src/Hooks/useAuth.js
+++ client/src/Hooks/useAuth.js
@@ -1,104 +1,134 @@
-import React, { createContext, useState, useEffect, useContext } from 'react';
-import { useNavigate, Outlet, Navigate } from 'react-router-dom';
-import axios from './useAxios';
+/**
+ *
+ * context and providers are used for global state management
+ * (eg. access a variable from here to any component in the hierarchy)
+ *
+ */
 
-const AuthContext = createContext({
-  user: null,
-  login: () => {},
-  logout: () => {},
-  register: () => {},
-  isAuth: () => false,
-});
+import { createContext, useEffect, useState } from 'react';
 
-const AuthProvider = ({ children }) => {
-  const navigate = useNavigate();
-  const [user, setUser] = useState(null);
+// google console user_account client id
+const CLIENT_ID = '987122640202-cttf76h8r41jmu6fp13ihoun32nuigq1.apps.googleusercontent.com';
 
-  const login = async (data) => {
-    const { email, password } = data;
-    try {
-      const response = await axios.post(`/auth/login`, {
-        email: email,
-        password: password,
-      });
-      console.log(response.data);
-      setUser(response.data);
-      localStorage.setItem('user', JSON.stringify(response.data));
-      navigate('/profile');
-    } catch (error) {
-      console.log(error);
-      alert(error.message);
-    }
-  };
+// context allows for the state to be accessed anywhere in the application
+const AccountContext = createContext();
 
-  const register = async (data) => {
-    const { name, first, middle, last, extension, sex, birthday, address, contact, about, email, password, role } = data;
+/**
+ * This is a custom Google Login Button that's hidden from the user.
+ * It's used to trigger the Google Login Popup.
+ *
+ */
+function HiddenGoogleButton() {
+	const googleLoginWrapper = document.createElement('div');
+	// Or you can simple hide it in CSS rule for custom-google-button
+	googleLoginWrapper.style.display = 'none';
+	googleLoginWrapper.classList.add('custom-google-button');
 
-    try {
-      const response = await axios.post(`/auth/register`, {
-        name: name,
-        firstName: first,
-        middleName: middle,
-        lastName: last,
-        extensionName: extension,
-        sex: sex,
-        birthday: birthday,
-        address: address || ' ',
-        contact: contact || ' ',
-        about: about || ' ',
-        email: email,
-        password: password,
-        role: role,
-      });
-      console.log(response);
-      setUser(response.data.user);
-      localStorage.setItem('user', JSON.stringify(response.data.user));
-      navigate('/dashboard');
-    } catch (error) {
-      console.log(error);
-      alert(error.message);
-    }
-  };
+	// Add the wrapper to body
+	document.body.appendChild(googleLoginWrapper);
 
-  const logout = async () => {
-    await axios.post(`/auth/logout`);
-    localStorage.clear();
-    navigate('/');
-  };
+	// Use GSI javascript api to render the button inside our wrapper
+	// You can ignore the properties because this button will not appear
+	window.google.accounts.id.renderButton(googleLoginWrapper, {});
 
-  const isAuth = (id) => {
-    if (!user) {
-      return false;
-    }
-    return true;
-  };
+	const googleLoginWrapperButton = googleLoginWrapper.querySelector('div[role=button]');
 
-  useEffect(() => {}, []);
+	return {
+		click: () => {
+			googleLoginWrapperButton.click();
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout, register, isAuth }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+			// Get all elements with class="custom-google-button"
+			var elementsToRemove = document.querySelectorAll('.custom-google-button');
 
-const useAuth = () => {
-  return useContext(AuthContext);
-};
+			// Iterate through the NodeList and remove each element
+			for (var i = 0; i < elementsToRemove.length; i++) {
+				elementsToRemove[i].remove();
+			}
+		},
+	};
+}
 
-const ProtectedRoute = ({ allowedRoles }) => {
-  const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : '';
-  const type = user.type;
+// provider is a wrapper for the entire application
+function AccountProvider({ children }) {
+	const [isLoggedIn, setIsLoggedIn] = useState(null);
+	const [route, setRoute] = useState(null);
+	const token = localStorage.getItem('token');
 
-  return user ? (
-    allowedRoles ? (
-      allowedRoles.includes(type) ? <Outlet /> : <Navigate to="/forbidden" />
-    ) : (
-      <Outlet />
-    )
-  ) : (
-    <Navigate to="/login" />
-  );
-};
+	// Callback handler when google sign in is successful
+	useEffect(() => {
+		// load and initialize the user_account from the Google Cloud Console
+		window.google.accounts.id.initialize({
+			/**
+			 * auto select user account
+			 * (used by the one-tap sign-up prompt when the existing token has expired or invalid)
+			 *
+			 */
 
-export { AuthProvider, useAuth, ProtectedRoute };
+			auto_select: true,
+			client_id: CLIENT_ID,
+
+			// login handler callback
+			callback: res => {
+				setIsLoggedIn(true);
+
+				// set the token in local storage
+				localStorage.setItem('token', res.credential);
+				console.log('Google Login Success', res.credential);
+			},
+		});
+
+		if (!token) {
+			setIsLoggedIn(false);
+			return;
+		}
+
+		fetch('http://localhost:5000/api/account/authenticate', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then(res => {
+				if (!res.ok) {
+					throw new Error(`HTTP error! Authentication Status: ${res.status}`);
+				}
+				return res.json();
+			})
+			.then(data => {
+				sessionStorage.setItem('auth', data);
+				setIsLoggedIn(true);
+			})
+			.catch(error => {
+				console.log(error);
+				/**
+				 * shows the one-tap sign-in prompt
+				 * auto selects the account since auto_select is set to true in Account Provider
+				 *
+				 */
+				setIsLoggedIn(false);
+				window.google.accounts.id.prompt();
+			});
+	}, []);
+
+	const Account = {
+		// handled by the account provider
+		isLoggedIn: isLoggedIn,
+
+		// handled by the router component
+		route: route,
+		setRoute: setRoute,
+
+		// context functions
+		login: () => HiddenGoogleButton().click(),
+		logout: () => {
+			window.google.accounts.id.disableAutoSelect();
+			localStorage.clear();
+			setIsLoggedIn(false);
+		},
+	};
+
+	return <AccountContext.Provider value={{ Account }}>{children}</AccountContext.Provider>;
+}
+
+export { AccountProvider, AccountContext };
